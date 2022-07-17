2ab36c4588ae20e71e0960205a78ce66b8e01e27
ertugrullberber <ertugrulberber3461@gmail.com>
2022-07-17 16:16:55 +0300

    KonumAyarlari, HaritayiOynatmak, Annotation

--- HaritalarUygulamasi/ViewController.swift
+++ HaritalarUygulamasi/ViewController.swift
@@ -9,13 +9,47 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class ViewController: UIViewController, MKMapViewDelegate {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(konumSec(gestureRecognizer:)))
+        gestureRecognizer.minimumPressDuration = 3
+        mapView.addGestureRecognizer(gestureRecognizer)
+        
+    }
+    
+    @objc func konumSec(gestureRecognizer : UILongPressGestureRecognizer) {
+        if gestureRecognizer.state == .began {
+            let dokunulanNokta = gestureRecognizer.location(in: mapView)
+            let dokunulanKoordinat = mapView.convert(dokunulanNokta,toCoordinateFrom: mapView)
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = dokunulanKoordinat
+            annotation.title = "Kullanici secimi"
+            annotation.subtitle = "ornek altyazi"
+            mapView.addAnnotation(annotation)
+        }
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        print(locations[0].coordinate.latitude)
+//        print(locations[0].coordinate.longitude)
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.9, longitudeDelta: 0.9)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
     }
 
 
